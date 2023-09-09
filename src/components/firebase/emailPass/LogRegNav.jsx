54945f0d6e4ed0212b0cdb4ce05dfe41e6ad417e
abuabddullah <asifaowadud@gmail.com>
2023-09-09 23:51:56 +0600

    std fire route context done

--- src/components/firebase/emailPass/LogRegNav.jsx
+++ src/components/firebase/emailPass/LogRegNav.jsx
@@ -6,7 +6,7 @@ const LogRegNav = () => {
     <nav>
       <ActiveLink to="/firebase/emailPassLogin/register">Register</ActiveLink>{" "}
       <ActiveLink to="/firebase/emailPassLogin/login">Login</ActiveLink>{" "}
-      <ActiveLink to="/firebase/emailPassLogin/profile">Profile</ActiveLink>{" "}
+      <ActiveLink to="/firebase/emailPassLogin/profile">ProtectedProfile</ActiveLink>{" "}
     </nav>
   );
 };
